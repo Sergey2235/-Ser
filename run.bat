cb0132883ea86bf6a3a56e67efc1c48730155851
Sergey2235 <seregas0207@gmail.com>
2026-03-03 18:50:12 +0500

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-python main.py
-if errorlevel 1 (
-    echo.
-    echo Ошибка при запуске. Проверьте, что Python установлен и все файлы на месте.
-    pause
-)
